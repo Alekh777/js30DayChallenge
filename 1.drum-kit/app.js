b4ac62a71010dac661a42068b0db4e4a501a658b
Alekh <alekhkumar777@gmail.com>
2021-05-24 17:38:07 +0530

    drum-kit: Add functionality

--- 1.drum-kit/app.js
+++ 1.drum-kit/app.js
@@ -1,10 +1,26 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    
-    if(!audio)
-        return;
+function playSound(e) {
+  let dataVal;
+  if (e.type === "mousedown") dataVal = this.getAttribute("data-key");
+  else dataVal = e.keyCode;
 
-    audio.currentTime = 0;
-    audio.play();
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-})
+  const audio = document.querySelector(`audio[data-key="${dataVal}"]`);
+
+  const key = document.querySelector(`div[data-key="${dataVal}"]`);
+
+  if (!audio) return;
+  
+  key.classList.add("playing");
+
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
+keys.forEach((key) => key.addEventListener("mousedown", playSound));
