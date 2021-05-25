9db32e9c93b7e5d4b4813cf97abc6251bd7bd700
Alekh <alekhkumar777@gmail.com>
2021-05-25 16:11:17 +0530

    css-js-clock: Fix transition of hands

--- 2.css-js-clock/script.js
+++ 2.css-js-clock/script.js
@@ -1,4 +1,5 @@
 function setDate(){
+    let hands = document.querySelectorAll('.hand');
     let secondHand = document.querySelector('.sec-hand');
     let minuteHand = document.querySelector('.min-hand');
     let hourHand = document.querySelector('.hr-hand');
@@ -7,16 +8,20 @@ function setDate(){
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    console.log(`${hours}:${minutes}:${seconds}`);
+
+    if(seconds === 0) hands.forEach(h => h.classList.remove('transition'))
+
     const secondsDegree = seconds * (360/60) + 90;
     const minutesDegree = minutes * (360/60) + 90;
     const hoursDegree = hours * (360/12) + 90;
 
     secondHand.style.transform = `translateY(-50%) rotate(calc(${secondsDegree}deg))`;
     
-    minuteHand.style.transform = `translaqteY(-50%) rotate(calc(${minutesDegree}deg))`;
+    minuteHand.style.transform = `translateY(-50%) rotate(calc(${minutesDegree}deg))`;
     
     hourHand.style.transform = `translateY(-50%) rotate(calc(${hoursDegree}deg))`;
+
+    if(seconds !== 0) hands.forEach(h => h.classList.add('transition'))
 }
 
 setInterval(setDate, 1000);
