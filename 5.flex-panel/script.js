56b452195aaf1c4384fb019a7f225c0488198b15
Alekh <alekhkumar777@gmail.com>
2021-06-02 20:27:08 +0530

    flex-panel: Transition first and last text child

--- 5.flex-panel/script.js
+++ 5.flex-panel/script.js
@@ -4,4 +4,12 @@ function addOpen(){
     this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+      this.classList.toggle('open-active');
+    }
+}
+
 panel.forEach(p => p.addEventListener('click', addOpen));
+panel.forEach(panel => panel.addEventListener('transitionend', toggleActive));
