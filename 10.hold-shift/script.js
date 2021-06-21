23b803cdd3ed199333ca950392f309fb547670b4
Alekh <alekhkumar777@gmail.com>
2021-06-21 20:01:12 +0530

    hold-shift: Listen for shift press

--- 10.hold-shift/script.js
+++ 10.hold-shift/script.js
@@ -1,5 +1,31 @@
+let index = [-1, -1];
+
+let shiftPressed = false;
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Shift")
+        shiftPressed = true;
+});
+
+document.addEventListener("keyup", (e) => {
+    if (e.key === "Shift")
+        shiftPressed = false;
+});
+
 const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
 checkBoxes.forEach((checkBox, i) => {
     checkBox.addEventListener("click", function checked(e){
+        if(shiftPressed){
+            if(index[0] == -1) {
+                console.log("i put", i);
+                index[0] = i;
+            }
+            else{
+                 index[1] = i;
+                console.log("i put", i);
+            }
+            console.log("Clicked and Shift Pressed")
+            if(index[1] != -1)
+                markAll();
+        }
     });
 });
